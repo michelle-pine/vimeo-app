072acbaa8be02db6e31de24d377be22448529884
Michelle <michelle.pine33@gmail.com>
2018-12-25 14:16:44 -0500

    Updates

---
+++ precache-manifest.8e1f0fa0c153807025abfc7b62be76a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a157af8a28f9a1137e",
+    "url": "/vimeo-app/static/js/runtime~main.a1a157af.js"
+  },
+  {
+    "revision": "3ebb87c3576d754db614",
+    "url": "/vimeo-app/static/js/main.3ebb87c3.chunk.js"
+  },
+  {
+    "revision": "0ff3cfc282232f147672",
+    "url": "/vimeo-app/static/js/1.0ff3cfc2.chunk.js"
+  },
+  {
+    "revision": "3ebb87c3576d754db614",
+    "url": "/vimeo-app/static/css/main.a220a947.chunk.css"
+  },
+  {
+    "revision": "c80a441f927a9e83ff9c489b58b65139",
+    "url": "/vimeo-app/index.html"
+  }
+];
